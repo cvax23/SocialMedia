a6555ebbe193b6d9a11c91a641de7598563f2748
Sebastian Reza <jsrezam@outlook.com>
2020-10-17 14:23:27 -0500

    Agregar archivos de proyecto.

---
+++ SocialMedia.Infrastructure/Data/SocialMediaContext.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using SocialMedia.Core.Entities;
+
+namespace SocialMedia.Infrastructure.Data
+{
+    public partial class SocialMediaContext : DbContext
+    {
+        public SocialMediaContext()
+        {
+        }
+
+        public SocialMediaContext(DbContextOptions<SocialMediaContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Comment> Comments { get; set; }
+        public virtual DbSet<Post> Posts { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Comment>(entity =>
+            {
+                entity.ToTable("Commentario");
+
+                entity.HasKey(e => e.CommentId);
+
+                entity.Property(e => e.CommentId)
+                .HasColumnName("IdComentario")
+                .ValueGeneratedNever();
+
+                entity.Property(e => e.Description)
+                .HasColumnName("Descripcion")
+                .IsRequired()                
+                .HasMaxLength(500)
+                .IsUnicode(false);
+
+                entity.Property(e => e.Date)
+                .HasColumnName("Fecha")
+                .HasColumnType("datetime");
+
+                entity.HasOne(d => d.Post)
+                    .WithMany(p => p.Comments)
+                    .HasForeignKey(d => d.PostId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Comentario_Publicacion");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Comments)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Comentario_Usuario");
+            });
+
+            modelBuilder.Entity<Post>(entity =>
+            {
+                entity.HasKey(e => e.PostId);
+
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasMaxLength(1000)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Date).HasColumnType("datetime");
+
+                entity.Property(e => e.Image)
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Posts)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Publicacion_Usuario");
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.UserId);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.BirthDate).HasColumnType("date");
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Phone)
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+            });
+
+            //OnModelCreatingPartial(modelBuilder);
+        }
+
+        //partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialMedia.Core.Entities;
+using SocialMedia.Core.Interfaces;
+using SocialMedia.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly SocialMediaContext _context;
+        public PostRepository(SocialMediaContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Post>> GetPosts() 
+        {
+            var posts = await _context.Posts.ToListAsync();
+            //MOCK
+            //var post = Enumerable.Range(1, 10).Select(x => new Post
+            //{
+            //    PostId = x,
+            //    Description = $"Description {x}",
+            //    Date = DateTime.Now,
+            //    Image = $"https://misapis.com/{x}",
+            //    UserId = x * 2,
+            //});
+            //await Task.Delay(10);
+            return posts;
+        }
+    }
+}
