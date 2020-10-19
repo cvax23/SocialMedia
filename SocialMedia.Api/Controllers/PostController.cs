980710a75c5d33658ea65c8e9d3b1c4104062f87
Sebastian Reza <jsrezam@outlook.com>
2020-10-19 01:43:39 -0500

    CRUD Operations with EF Core

--- SocialMedia.Api/Controllers/PostController.cs
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Api.Responses;
 using SocialMedia.Core.DTOs;
 using SocialMedia.Core.Entities;
 using SocialMedia.Core.Interfaces;
@@ -15,17 +16,18 @@ namespace SocialMedia.Api.Controllers
         private readonly IPostRepository _postRepository;
         private readonly IMapper _mapper;
 
-        public PostController(IPostRepository postRepository,IMapper mapper)
+        public PostController(IPostRepository postRepository, IMapper mapper)
         {
             _postRepository = postRepository;
             _mapper = mapper;
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetPosts() 
+        public async Task<IActionResult> GetPosts()
         {
             var posts = await _postRepository.GetPosts();
             var postsDto = _mapper.Map<IEnumerable<PostDto>>(posts);
+            var response = new ApiResponse<IEnumerable<PostDto>>(postsDto);
             return Ok(postsDto);
         }
 
@@ -34,18 +36,36 @@ namespace SocialMedia.Api.Controllers
         {
             var post = await _postRepository.GetPost(id);
             var postDto = _mapper.Map<PostDto>(post);
-                
-            return Ok(postDto);
+            var response = new ApiResponse<PostDto>(postDto);
+            return Ok(response);
         }
 
         [HttpPost]
         public async Task<IActionResult> Post(PostDto postDto)
         {
-            var post = _mapper.Map<Post>(postDto);            
+            var post = _mapper.Map<Post>(postDto);
             await _postRepository.InsertPost(post);
-            postDto.PostId = post.PostId;
+            postDto = _mapper.Map<PostDto>(post);
+            var response = new ApiResponse<PostDto>(postDto);
+            return Ok(response);
+        }
 
-            return Ok(postDto);
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id,PostDto postDto)
+        {
+            var post = _mapper.Map<Post>(postDto);
+            post.PostId = id;
+            var result = await _postRepository.UpdatePost(post);
+            var response = new ApiResponse<bool>(result);
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _postRepository.DeletePost(id);
+            var response = new ApiResponse<bool>(result);
+            return Ok(response);
         }
     }
 }
--- SocialMedia.Core/Interfaces/IPostRepository.cs
+++ SocialMedia.Core/Interfaces/IPostRepository.cs
@@ -9,10 +9,10 @@ namespace SocialMedia.Core.Interfaces
     public interface IPostRepository
     {
         Task<IEnumerable<Post>> GetPosts();
-
         Task<Post> GetPost(int id);
-
         Task InsertPost(Post post);
+        Task<bool> UpdatePost(Post post);
+        Task<bool> DeletePost(int id);
 
     }
 }
